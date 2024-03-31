9f09d14551cdb923a79d981c0504460f4cc5b369
BloodTroller <ferlan.cesnik@gmail.com>
2024-03-31 18:27:38 +0200

    Added example code for showing video and some comments.

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread("Sources/still_image.jpg")
-#default cvtcolor = bgr
+## SHOW IMAGE
+# img = cv.imread("Sources/still_image.jpg")
+## default cvtcolor = bgr
+# img = cv.resize(img, (220, 340))
+## resize to desired
+#
+# cv.imshow("frame", img)
+#
+# cv.waitKey(0)
 
-img = cv.resize(img, (220, 340))
-#resize to desired
+## CAMERA CAPTURE
+# cam = cv.VideoCapture(0)
+#
+# while True:
+#     ret, frame = cam.read()
+#     frame = cv.flip(frame, 1)
+#
+#     cv.imshow("Camera (press Q to close)", frame)
+#
+#     if cv.waitKey(1) & 0xFF == ord('q'):
+#         break
+#
+# cam.release()
 
-cv.imshow("frame", img)
+## SHOW VIDEO
+vid = cv.VideoCapture("Sources/little_movement.mp4")
+# ms between frames info; https://fpstoms.com
+frameTime = 40
 
-cv.waitKey(0)
+while True:
+    check, frame = vid.read()
 
+    if check:
+        cv.imshow("cam_frame", frame)
+        cv.setWindowTitle("cam_frame", "Camera (press Q to close)")
+
+    if cv.waitKey(1) == ord('q') & cv.waitKey(frameTime):
+        break
+
+#release camera
+vid.release()
+
+#end cv2
 cv.destroyAllWindows()
