f6d53884f77b9c48ee23b753eac77b6b34922f56
BloodTroller <ferlan.cesnik@gmail.com>
2024-03-31 18:31:32 +0200

    Decided to only use video for detection for now (removed all that annoying example comments)

--- main.py
+++ main.py
@@ -1,30 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-## SHOW IMAGE
-# img = cv.imread("Sources/still_image.jpg")
-## default cvtcolor = bgr
-# img = cv.resize(img, (220, 340))
-## resize to desired
-#
-# cv.imshow("frame", img)
-#
-# cv.waitKey(0)
-
-## CAMERA CAPTURE
-# cam = cv.VideoCapture(0)
-#
-# while True:
-#     ret, frame = cam.read()
-#     frame = cv.flip(frame, 1)
-#
-#     cv.imshow("Camera (press Q to close)", frame)
-#
-#     if cv.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# cam.release()
-
 ## SHOW VIDEO
 vid = cv.VideoCapture("Sources/little_movement.mp4")
 # ms between frames info; https://fpstoms.com
@@ -33,11 +9,11 @@ frameTime = 40
 while True:
     check, frame = vid.read()
 
-    if check:
+    if check & cv.waitKey(frameTime):
         cv.imshow("cam_frame", frame)
         cv.setWindowTitle("cam_frame", "Camera (press Q to close)")
 
-    if cv.waitKey(1) == ord('q') & cv.waitKey(frameTime):
+    if cv.waitKey(1) == ord('q'):
         break
 
 #release camera
