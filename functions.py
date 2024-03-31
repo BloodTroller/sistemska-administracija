Added skin color selection and calculation. Fixed minor input bugs for closing the video and such

functions.py
@@ -1,6 +1,15 @@
-def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
-    pass
+import cv2
+import numpy as np
 
+
+def doloci_barvo_koze(slika, levo_zgoraj, desno_spodaj):
+    x1, y1 = levo_zgoraj
+    x2, y2 = desno_spodaj
+    izbira = slika[y1:y2, x1:x2]
+    b = np.mean(izbira[:, :, 0])
+    g = np.mean(izbira[:, :, 1])
+    r = np.mean(izbira[:, :, 2])
+    return [b, g, r]
 def zmanjsaj_sliko(slika,sirina,visina):
     pass
 