135038f53c4d74e3e5a2260e92abcd00dc9b3049
BloodTroller <ferlan.cesnik@gmail.com>
2024-03-31 20:37:16 +0200

    Added FPS counter and fixed minor input bug - again.

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ def tocke_slike(event, x, y, flags, params):
         else:
             if len(cord) == 0:
                 cord.append([x, y])
-                displaystring = "Camera (press ENTER to confirm selection - 2 corners [(" + str(cord[0][0]) + "," + str(cord[0][1]) + "), Y ])"
+                displaystring = "Camera (press ENTER to confirm selection - 2 corners [(" + str(cord[0][0]) + "," + str(
+                    cord[0][1]) + "), Y ])"
                 cv.setWindowTitle("cam_frame", displaystring)
                 print(displaystring)
             if cord[0] != [x, y] and len(cord) == 1:
@@ -28,14 +29,17 @@ def tocke_slike(event, x, y, flags, params):
                 a1, b1 = cord[0]
                 a2, b2 = cord[1]
                 for index in range(a1, a2):
-                    pframe[b1, index] = [255 - frame[b1, index, 0], 255 - frame[b1, index, 1], 255 - frame[b1, index, 2]]
-                    pframe[b2, index] = [255 - frame[b2, index, 0], 255 - frame[b2, index, 1], 255 - frame[b2, index, 2]]
+                    pframe[b1, index] = [255 - frame[b1, index, 0], 255 - frame[b1, index, 1],
+                                         255 - frame[b1, index, 2]]
+                    pframe[b2, index] = [255 - frame[b2, index, 0], 255 - frame[b2, index, 1],
+                                         255 - frame[b2, index, 2]]
 
                 for ind in range(b1, b2):
                     pframe[ind, a1] = [255 - frame[ind, a1, 0], 255 - frame[ind, a1, 1], 255 - frame[ind, a1, 2]]
                     pframe[ind, a2] = [255 - frame[ind, a2, 0], 255 - frame[ind, a2, 1], 255 - frame[ind, a2, 2]]
 
-                displaystring = "Camera (press ENTER to confirm selection - 2 corners [(" + str(cord[0][0]) + "," + str(cord[0][1]) + "), (" + str(cord[1][0]) + "," + str(cord[1][1]) + ")])"
+                displaystring = "Camera (press ENTER to confirm selection - 2 corners [(" + str(cord[0][0]) + "," + str(
+                    cord[0][1]) + "), (" + str(cord[1][0]) + "," + str(cord[1][1]) + ")])"
                 cv.setWindowTitle("cam_frame", displaystring)
                 print(displaystring)
 
@@ -76,37 +80,51 @@ def polepsaj_tocke():
 
 
 ## SHOW VIDEO
-<<<<<<< HEAD
-vid = cv.VideoCapture("Sources/little_movement.mp4")
-cv.namedWindow("cam_frame")
-=======
 vid = cv.VideoCapture(0)
 cv.namedWindow("cam_frame")
 cv.setMouseCallback("cam_frame", tocke_slike)
->>>>>>> detection-develop
 # ms between frames info; https://fpstoms.com
-frameTime = 40
 waitForSelection = True
 play = True
 cord = []
 cv.setWindowTitle("cam_frame", "Camera (press Q to close)")
 print("Camera (press Q to close)")
 abc = True
+displayFont = cv.FONT_HERSHEY_DUPLEX
+prev_frame_time = 0
+new_frame_time = 0
 
 while play and cv.getWindowProperty("cam_frame", cv.WND_PROP_VISIBLE) >= 1:
-    check, frame = vid.read()
-    if check:
-        # VELIKOST SLIKE
-        frame = f.zmanjsaj_sliko(frame, 340, 220)
-        if abc:
-            cv.imshow("cam_frame", frame)
-            abc = False
-    else:
+    check, fr = vid.read()
+    frame = fr
+    new_frame_time = time.time()
+    fps = 1 / (new_frame_time - prev_frame_time)
+    prev_frame_time = new_frame_time
+    fps = str(int(fps))
+    frame = cv.flip(frame, 1)
+    cv.putText(frame, fps, (7, 70), displayFont, 2, (0, 0, 255), 5, cv.LINE_AA)
+
+    if not check:
         print("Video ended")
         play = False
         break
+    elif check and waitForSelection:
+        # VELIKOST SLIKE
+        # frame = f.zmanjsaj_sliko(frame, 340, 220)
+        # frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+        if abc:
+            # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            cv.imshow("cam_frame", frame)
+            abc = False
+    else:
+        # print(f.prestej_piksle_z_barvo_koze(frame, barva_koze))
+        squares = f.obdelaj_sliko_s_skatlami(frame, 5, 5, barva_koze)
+        # print(squares)
+        for i in range(len(squares)):
+            cv.rectangle(frame, (squares[i][0], squares[i][1]), (squares[i][2], squares[i][3]), (100, 100, 100), 1)
+        cv.imshow("cam_frame", frame)
 
-    if cv.waitKey(frameTime) == ord('q') or cv.getWindowProperty("cam_frame", cv.WND_PROP_VISIBLE) < 1:
+    if cv.waitKey(1) == ord('q') or cv.getWindowProperty("cam_frame", cv.WND_PROP_VISIBLE) < 1:
         play = False
         break
 
@@ -121,20 +139,13 @@ while play and cv.getWindowProperty("cam_frame", cv.WND_PROP_VISIBLE) >= 1:
             barva_koze_min, barva_koze_max = f.doloci_barvo_koze(frame, cord[0], cord[1])
             print("Min: " + str(barva_koze_min) + " Max: " + str(barva_koze_max))
             barva_koze = np.array([barva_koze_min, barva_koze_max])
+            cv.setWindowTitle("cam_frame", "Camera")
         if k == ord('q') or cv.getWindowProperty("cam_frame", cv.WND_PROP_VISIBLE) < 1:
             play = False
             waitForSelection = False
             cv.setWindowTitle("cam_frame", "Camera (press Q to close)")
             print("Camera (press Q to close)")
             break
-
-    if check:
-        squares = f.obdelaj_sliko_s_skatlami(frame, 10, 10, barva_koze)
-        print(squares)
-        for i in range(len(squares)):
-            cv.rectangle(frame, (squares[i][0], squares[i][1]), (squares[i][2], squares[i][3]), (100, 100, 100), 1)
-        cv.imshow("cam_frame", frame)
-
 
 # release camera
 vid.release()
