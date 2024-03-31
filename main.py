98a6fc6e54083c73816a8eb48b52807d57253777
BloodTroller <ferlan.cesnik@gmail.com>
2024-03-31 18:45:28 +0200

    Window now automatically closes after the video has ended. Fixed a problem where 'Q' would sometimes not be registered and the video would not end. Fixed the bug where the window could not be closed with the X button.

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import numpy as np
 
 ## SHOW VIDEO
 vid = cv.VideoCapture("Sources/little_movement.mp4")
+cv.namedWindow("cam_frame")
 # ms between frames info; https://fpstoms.com
 frameTime = 40
 
-while True:
+while True and cv.getWindowProperty("cam_frame", cv.WND_PROP_VISIBLE) >= 1:
     check, frame = vid.read()
 
-    if check & cv.waitKey(frameTime):
+    if check:
         cv.imshow("cam_frame", frame)
         cv.setWindowTitle("cam_frame", "Camera (press Q to close)")
+    else:
+        print("Video ended")
+        play = False
+        break
 
-    if cv.waitKey(1) == ord('q'):
+    if cv.waitKey(frameTime) == ord('q') or cv.getWindowProperty("cam_frame", cv.WND_PROP_VISIBLE) < 1:
         break
 
 #release camera
