2f3268acc459569b26edd51d8577fc6282ce677b
BloodTroller <ferlan.cesnik@gmail.com>
2024-04-14 19:23:44 +0200

    Added tests

---
+++ tests/functions_test.py
@@ -0,0 +1,13 @@
+import functions
+import cv2 as cv
+def test_prestej_piksle_z_barvo_koze():
+    pic = cv.imread("face.png")
+    assert functions.prestej_piksle_z_barvo_koze(pic, [(176, 228, 239),(176, 228, 239)]) == 8418
+
+def test_zmanjsaj_sliko():
+    pic = cv.imread("face.png")
+    assert functions.zmanjsaj_sliko(pic, 200, 100) == cv.resize(pic, (200, 100))
+
+def test_doloci_barvo_koze():
+    pic = cv.imread("face.png")
+    assert functions.doloci_barvo_koze(pic,[0, 0], [pic.shape[0]-1, pic.shape[1]-1]) == ((176, 228, 239), (87, 122, 185))
