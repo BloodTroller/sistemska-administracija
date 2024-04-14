85bddfbef9bebeba18c1253c758c2b624a6d577b
BloodTroller <ferlan.cesnik@gmail.com>
2024-04-14 19:38:34 +0200

    Fixed functions test

--- tests/functions_test.py
+++ tests/functions_test.py
@@ -1,13 +1,18 @@
-from .. import functions as f
 import cv2 as cv
+import sys
+
+import numpy as np
+
+sys.path.insert(0, '../')
+import functions as f
 def test_prestej_piksle_z_barvo_koze():
     pic = cv.imread("face.png")
     assert f.prestej_piksle_z_barvo_koze(pic, [(176, 228, 239),(176, 228, 239)]) == 8418
 
 def test_zmanjsaj_sliko():
     pic = cv.imread("face.png")
-    assert f.zmanjsaj_sliko(pic, 200, 100) == cv.resize(pic, (200, 100))
+    assert np.array_equal(f.zmanjsaj_sliko(pic, 200, 100), cv.resize(pic, (200, 100)))
 
 def test_doloci_barvo_koze():
     pic = cv.imread("face.png")
-    assert f.doloci_barvo_koze(pic,[0, 0], [pic.shape[0]-1, pic.shape[1]-1]) == ((176, 228, 239), (87, 122, 185))
+    assert np.array_equal(f.doloci_barvo_koze(pic,[0, 0], [pic.shape[0]-1, pic.shape[1]-1]), ([87, 122, 185], [176, 228, 239]))
