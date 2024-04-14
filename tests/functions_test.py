087b13ea21291505ec2d580c2d3472bcf248ae31
BloodTroller <ferlan.cesnik@gmail.com>
2024-04-14 19:27:46 +0200

    Test commit (changed functions import inside functions_test)

--- tests/functions_test.py
+++ tests/functions_test.py
@@ -1,13 +1,13 @@
-import functions
+from .. import functions as f
 import cv2 as cv
 def test_prestej_piksle_z_barvo_koze():
     pic = cv.imread("face.png")
-    assert functions.prestej_piksle_z_barvo_koze(pic, [(176, 228, 239),(176, 228, 239)]) == 8418
+    assert f.prestej_piksle_z_barvo_koze(pic, [(176, 228, 239),(176, 228, 239)]) == 8418
 
 def test_zmanjsaj_sliko():
     pic = cv.imread("face.png")
-    assert functions.zmanjsaj_sliko(pic, 200, 100) == cv.resize(pic, (200, 100))
+    assert f.zmanjsaj_sliko(pic, 200, 100) == cv.resize(pic, (200, 100))
 
 def test_doloci_barvo_koze():
     pic = cv.imread("face.png")
-    assert functions.doloci_barvo_koze(pic,[0, 0], [pic.shape[0]-1, pic.shape[1]-1]) == ((176, 228, 239), (87, 122, 185))
+    assert f.doloci_barvo_koze(pic,[0, 0], [pic.shape[0]-1, pic.shape[1]-1]) == ((176, 228, 239), (87, 122, 185))
