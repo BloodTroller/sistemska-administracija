5dd6d8b5a55ca00ee89b1436d11537ac54f6434f
BloodTroller <ferlan.cesnik@gmail.com>
2024-04-16 13:08:17 +0200

    added display parameters (just a test)

--- zapakiraj_27105.sh
+++ zapakiraj_27105.sh
@@ -1,3 +1,3 @@
 
-docker build . --file Dockerfile --tag bloodtroller/sistemska-administracija:latest
+docker build . --file Dockerfile --runtime nvidia -e DISPLAY=$DISPLAY --tag bloodtroller/sistemska-administracija:latest
 docker push bloodtroller/sistemska-administracija:latest
