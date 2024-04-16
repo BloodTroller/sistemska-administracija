9a604d86bf60b49677a8fa84510072292d5c2cf8
BloodTroller <ferlan.cesnik@gmail.com>
2024-04-16 08:38:05 +0200

    updated zazeni script

--- zazeni_27105.sh
+++ zazeni_27105.sh
@@ -1,5 +1,5 @@
 mkdir images
 
-docker run -v /pics --name "docker_image" -ti bloodtroller/sistemska-administracija
+docker run -v /images  --name "docker_image" -ti bloodtroller/sistemska-administracija
 
 docker rm images/
