66d9a0215c034afa13680022345c4b71750ee77a
BloodTroller <ferlan.cesnik@gmail.com>
2024-04-16 09:59:44 +0200

    added privileged to docker run

--- zazeni_27105.sh
+++ zazeni_27105.sh
@@ -1,3 +1,3 @@
 docker rm docker_image
-docker run --name "docker_image" -ti bloodtroller/sistemska-administracija
+docker run --privileged --name "docker_image" -ti bloodtroller/sistemska-administracija
 docker rm docker_image
